c3617f3ef8c2d0eb3bd84a2c93db65c77dff92ef
paco <prosich@gmail.com>
2021-09-21 00:32:38 +0200

    zz

--- src/hola.cpp
+++ src/hola.cpp
@@ -2,15 +2,19 @@
 #include <Espini.h>
 #include "config.h"
 #include "ver.h"
+#include <iostream>
+#include <string>
 #include <ESP8266WebServer.h>
 #include "EmonLib.h"
 
 EnergyMonitor    emon;
 ESP8266WebServer server(80);
+Espini *esp;
 
 void responde() {
   char amp[90];
   snprintf(amp, sizeof amp,"%.2f",emon.calcIrms(1480));
+  esp->log(amp);
   server.send(200,"text/plain",amp);  
 }
 
@@ -19,7 +23,7 @@ void reset() {
 }
 
 void setup() { 
-  Espini cosa(wifis(),"acmonitor",ver,otasrv,logsrv);
+  esp=new Espini(wifis(),"acmonitor",ver,otasrv,logsrv);
   emon.current(A0, 28.0); // Calibrado empirico
   server.on  ("/",responde);
   server.on  ("/reset",reset);
